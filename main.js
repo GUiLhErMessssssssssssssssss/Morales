405a4c0686865a1818ba091188c9e273674109e7
GUiLhErMessssssssssssssssss <140725028+GUiLhErMessssssssssssssssss@users.noreply.github.com>
2024-08-09 10:39:42 -0300

    Add files via upload
    questoes

---
+++ main.js
@@ -0,0 +1,13 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPerguntas = document.querySelector(.'caixaPerguntas');
+const caixaAlternativas = document.querySelector('caixa-alternativas');
+const caixaResultado = document.querySelector('caixa=-resultado');
+const Perguntas = [
+    {
+        enunciado: 'pergunta 1',
+        alternativas:[
+        'alternativa 1'   
+        'alternativa 2'
+        ]
+    }
+]
