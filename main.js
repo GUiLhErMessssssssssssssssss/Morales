aa4dc9de40fea2ff026ffd0e5921bbcab6a18373
GUiLhErMessssssssssssssssss <140725028+GUiLhErMessssssssssssssssss@users.noreply.github.com>
2024-08-14 07:51:46 -0300

    Add files via upload
    terminando codegos

--- main.js
+++ main.js
@@ -2,12 +2,20 @@ const caixaPrincipal = document.querySelector('.caixa-principal');
 const caixaPerguntas = document.querySelector(.'caixaPerguntas');
 const caixaAlternativas = document.querySelector('caixa-alternativas');
 const caixaResultado = document.querySelector('caixa=-resultado');
+const textoResultado = document.querySelector('.texto-resultado');
 const Perguntas = [
     {
-        enunciado: 'pergunta 1',
+        enunciado: 'Pergunta 1',
         alternativas:[
-        'alternativa 1'   
+        'alternativa 1',  
         'alternativa 2'
+        ]
+    },
+    {
+        enunciado: 'Pergunta 2'
+        alternativas:[
+            "Alternativa 1",
+            "Alternativa 2"
         ]
     }
 ]
